3f93dbb45c61bd4caf1e8792aa7706455cd6baa6
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2023-03-16 08:46:55 +0800

    add kpi for moonbase alpha testnet

---
+++ component/MoonbaseAlpha.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import BarChart from "./BarChart";
+import useSWR from "swr";
+
+//@ts-ignore
+const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json());
+
+const AlphaTest = () => {
+  const { data } = useSWR("/api/coraTransactions_moonbase", fetcher);
+
+  return (
+    <div className="text-lg w-3/4">
+      <h2 className="font-bold text-2xl w-full text-center mb-4">
+        Moonbase Alpha Testnet
+      </h2>
+      <div className="mb-4 grid grid-cols-3 gap-6 text-left w-1/2 mx-auto">
+        <div className="">
+          <p>
+            <span className="mr-2">Users:</span>
+            <span className="font-semibold">{data?.users?.length}</span>
+          </p>
+        </div>
+
+        <div>
+          <p>
+            <span className="mr-2">Deposits:</span>
+            <span className="font-semibold">{data?.deposits?.length}</span>
+          </p>
+
+          <p>
+            <span className="mr-2">Signal Withdraws:</span>
+            <span className="font-semibold">
+              {data?.signalWithdrawals?.length}
+            </span>
+          </p>
+
+          <p>
+            <span className="mr-2">Withdraws:</span>
+            <span className="font-semibold">{data?.withdraws?.length}</span>
+          </p>
+        </div>
+
+        <div>
+          <p>
+            <span className="mr-2">Borrows:</span>
+            <span className="font-semibold">{data?.borrows?.length}</span>
+          </p>
+
+          <p>
+            <span className="mr-2">Repays:</span>
+            <span className="font-semibold">{data?.repays?.length}</span>
+          </p>
+
+          <p>
+            <span className="mr-2">Collect collaterals:</span>
+            <span className="font-semibold">{data?.collects?.length}</span>
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <BarChart
+          deposits={data?.deposits}
+          borrows={data?.borrows}
+          repays={data?.repays}
+          signalWithdrawals={data?.signalWithdrawals}
+          withdraws={data?.withdraws}
+          collect={data?.collects}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AlphaTest;
---
+++ pages/moonbase.tsx
@@ -0,0 +1,11 @@
+import MoonbaseAlpha from "../component/MoonbaseAlpha";
+
+export default function Home() {
+  return (
+    <main className="p-10">
+      <div className="flex flex-col gap-20 w-screen justify-center">
+        <MoonbaseAlpha />
+      </div>
+    </main>
+  );
+}
